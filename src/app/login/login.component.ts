a6c8d545570c72d425bd640f5814ba1859e50336
Anousone Kaseumsouk, Hisato Kawaminami, Jean Jia and Lan Dam <lan@>
2018-08-27 16:34:17 -0700

    add login check

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  isLoggedIn: boolean = false;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(dataLastEmittedFromObserver => {
+      this.users = dataLastEmittedFromObserver;
+   });
+ }
+  checkLogin(userName, userPass){
+    console.log(this.users);
+    for(let i = 0; i < this.users.length; i++) {
+      if (this.users[i].username == userName) {
+        if (this.users[i].password == userPass) {
+          this.isLoggedIn = true;
+        }
+      }
+    }
+    if (this.isLoggedIn) {
+      // route to home
+      alert("Login is good");
+    } else {
+      alert("Incorrect username/password");
+    }
   }
 
 }
