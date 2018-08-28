459e4dbecd5672d59234842416f51abde1644d9a
Jean Jia and Steven Colburn <steven@>
2018-08-28 16:25:08 -0700

    can get posts, thank you john

---
+++ src/app/blog.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Blog, Post } from './models/blog';
+
+@Injectable()
+export class BlogService {
+  blogs: FirebaseListObservable<Blog[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.blogs = database.list('blogs');
+  }
+
+  getBlogs(){
+    return this.blogs;
+  }
+
+  addBlog(newBlog: Blog) {
+    this.blogs.push(newBlog);
+  }
+
+  getBlogByKey(blogKey: string){
+    return this.database.object('/blogs/' + blogKey);
+  }
+
+}
