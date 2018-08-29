270d88222822316681bb1d79e0662b7781a4748b
Jean Jia and Steven Colburn <steven@>
2018-08-29 15:11:34 -0700

    repair contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -17,16 +17,14 @@ export class ContactComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-     this.userService.getUsers().subscribe(dataLastEmitted => {
-        this.users = dataLastEmitted;
-        this.user = this.userService.getCurrentUser();
-        this.contacts = this.user.contacts;
-        for(let i = 0; i < this.user.contacts.length; i++) {
-          this.contacts[i] = this.users[parseInt(this.user.contacts[i])].username;
+    this.userService.getUsers().subscribe(dataLastEmitted => {
+      this.users = dataLastEmitted;
+      this.user = this.userService.getCurrentUser();
+      this.contacts = this.user.contacts;
+      for(let i = 0; i < this.user.contacts.length; i++) {
+        this.contacts[i] = this.users[parseInt(this.user.contacts[i])].username;
+      }
     });
-
-  }
-
-console.log(this.contacts);
-}
+    console.log(this.contacts);
+  }  
 }
