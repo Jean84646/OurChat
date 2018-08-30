80b3cb00a0e1e237408d92d19619d4a6de69f809
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-08-30 10:24:08 -0700

    Save merger edits.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,11 +9,7 @@ import { Router } from '@angular/router';
 })
 export class SignUpComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private userService: UserService, private router: Router){}
-=======
-  constructor(private userService: UserService, private router: Router) {}
->>>>>>> 9874b3feba2ab5274af354a24c0063be2a4acc03
 
   ngOnInit() {
   }
